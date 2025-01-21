945000fdfc0ebba737cb6d1f38d3409f64ad0862
Kirti Sharma <59800818+kirti2602@users.noreply.github.com>
2025-01-22 00:45:48 +0530

    Time: 0 ms (100%), Space: 45.6 MB (86.39%) - LeetHub

--- 0792-binary-search/0792-binary-search.java
+++ 0792-binary-search/0792-binary-search.java
@@ -3,28 +3,24 @@ class Solution {
         int start = 0;
         int end = nums.length - 1;
 
-        return search(nums, target, start,end);
+        return search(nums, target, start, end);
 
     }
 
-    static int search(int[] nums, int target, int start, int end){
-        if(start > end){
-            //not found
+    static int search(int[] nums, int target, int start, int end) {
+        if (start > end) {
+            // not found
             return -1;
         }
 
-        int mid = start + (end - start)/2;
+        int mid = start + (end - start) / 2;
 
-        if(target  == nums[mid]){
+        if (target == nums[mid]) {
             return mid;
+        } else if (target > nums[mid]) {
+            return search(nums, target, mid + 1, end);
         }
-        else if(target > nums[mid]){
-            return search(nums, target, mid+1, end);
-        }
-        else if(target < nums[mid]){
-            return search(nums, target, start, mid-1);
-        }
+        return search(nums, target, start, mid - 1);
 
-        return -1;
     }
 }
