d1ac43c7252bc2fbb4845450d88dff8e9621e83b
Kirti Sharma <59800818+kirti2602@users.noreply.github.com>
2025-02-16 00:35:29 +0530

    Time: 0 ms (100%), Space: 40.1 MB (92.63%) - LeetHub

--- 2507-number-of-common-factors/2507-number-of-common-factors.java
+++ 2507-number-of-common-factors/2507-number-of-common-factors.java
@@ -1,11 +1,20 @@
 class Solution {
     public int commonFactors(int a, int b) {
-        int smallestNum = a < b ? a : b;
+        int min = Math.min(a, b);
+        int max = Math.max(a, b);
         int count = 0;
 
-        for(int i = 1; i <= smallestNum; i++){
-            if(a % i == 0 && b % i == 0){
-                count++;
+        for(int i = 1; i <= Math.sqrt(min); i++){
+            if(min % i == 0){
+                if(max % i == 0){
+                    count++; // First divisor in!
+                }
+
+                int quotient = min / i;
+
+                if(quotient != i && max % quotient == 0){
+                    count++;
+                }
             }
         }
 
