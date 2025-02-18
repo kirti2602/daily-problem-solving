1983cf5773c46c8fa0a1c59a684231388d5ed8fa
Kirti Sharma <59800818+kirti2602@users.noreply.github.com>
2025-02-18 23:57:37 +0530

    Time: 2 ms (14.31%), Space: 47.1 MB (5.04%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,15 +1,27 @@
 class Solution {
     public void sortColors(int[] nums) {
-        
-        for(int i = 0; i<nums.length; i++){
-            for(int j = 1; j < nums.length - i; j++ ){
-                if(nums[j] < nums[j-1]){
-                    //swap
-                    int temp = nums[j];
-                    nums[j] = nums[j-1];
-                    nums[j-1] = temp;
-                }
+        bubbleSort(nums, 0, 0);
+
+    }
+
+    static void bubbleSort(int[] nums, int r, int c){
+        if(r == nums.length){
+            return;
+        }
+
+        if(c < nums.length-1){
+            if(nums[c] > nums[c+1]){
+                //swap
+                int temp = nums[c+1];
+                nums[c+1] = nums[c];
+                nums[c] = temp;
+
             }
+        bubbleSort(nums, r, c+1);
+        }else{
+            bubbleSort(nums, r+1, 0);
         }
     }
+
+
 }
