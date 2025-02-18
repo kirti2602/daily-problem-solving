14dbda0ba96af7e3fd6fa3d93a0b8e1eb71e50c0
Kirti Sharma <59800818+kirti2602@users.noreply.github.com>
2025-02-18 23:58:20 +0530

    Time: 2 ms (14.31%), Space: 44.1 MB (5.04%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,15 +1,15 @@
 class Solution {
     public void sortColors(int[] nums) {
-        bubbleSort(nums, 0, 0);
+        bubbleSort(nums, nums.length-1, 0);
 
     }
 
     static void bubbleSort(int[] nums, int r, int c){
-        if(r == nums.length){
+        if(r == 0){
             return;
         }
 
-        if(c < nums.length-1){
+        if(c < r){
             if(nums[c] > nums[c+1]){
                 //swap
                 int temp = nums[c+1];
@@ -19,7 +19,7 @@ class Solution {
             }
         bubbleSort(nums, r, c+1);
         }else{
-            bubbleSort(nums, r+1, 0);
+            bubbleSort(nums, r-1, 0);
         }
     }
 
